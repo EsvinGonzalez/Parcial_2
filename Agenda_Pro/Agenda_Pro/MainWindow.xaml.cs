240a6ed72e64635c5cac95e07eeff12199fac4c2
Esvin Gonzalez <egonzalezd12@miumg.edu.gt>
2023-04-29 22:43:49 -0600

    Agenda_y_codigo_animal_corregido

---
+++ Agenda_Pro/Agenda_Pro/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace AgendaBasica
+{
+    public partial class MainWindow : Window
+    {
+        private class Agenda_Pro
+        {
+            public string Nombre { get; set; }
+            public string Descripcion { get; set; }
+            public DateTime Fecha { get; set; }
+        }
+
+        private List<Agenda_Pro> notas = new List<Agenda_Pro>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Agregar_Click(object sender, RoutedEventArgs e)
+        {
+            Agenda_Pro tarea = new Agenda_Pro();
+            tarea.Nombre = txtNombre.Text;
+            tarea.Descripcion = txtDescripcion.Text;
+            tarea.Fecha = dpFecha.SelectedDate.GetValueOrDefault();
+
+            notas.Add(tarea);
+
+            lstTareas.Items.Add(tarea);
+        }
+
+        private void Eliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstTareas.SelectedItem != null)
+            {
+                Agenda_Pro tareaAEliminar = lstTareas.SelectedItem as Agenda_Pro;
+
+                notas.Remove(tareaAEliminar);
+
+                lstTareas.Items.Remove(tareaAEliminar);
+            }
+        }
+
+        private void lstTareas_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            Console.WriteLine("Nombre");
+        }
+
+        private void txtNombre_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            Console.Write(txtNombre.Text);
+        }
+    }
+}
---
+++ Serie_No_3/Serie_No_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_No_3
+{
+    internal class Program
+    {
+        private class Perro
+        {
+            public string Nombre { get; set; }
+            public int Edad { get; set; }
+
+            public void Comer()
+            {
+                Console.WriteLine("El perro está comiendo.");
+            }
+            public void Ladrar()
+            {
+                Console.WriteLine("Guau guau!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Perro miPerro = new Perro();
+            miPerro.Nombre = "Firulais";
+            miPerro.Edad = 3;
+
+            Console.WriteLine("Nombre del perro: " + miPerro.Nombre);
+            Console.WriteLine("Edad del perro: " + miPerro.Edad);
+            miPerro.Comer();
+
+            miPerro.Ladrar();
+        }
+    }
+}
